afb90ede81b0cd8cb9dbfbad7f9bfe45989526b0
BryanHafidz <nugroho31.com@gmail.com>
2020-11-16 18:04:03 +0700

    Create autokernel-AOSP.sh
    This Script for TG Shell-Bot

---
+++ autokernel-AOSP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clone Repo
+/r git clone https://github.com/MumetNgoding/BrynKernel-AOSP
+
+# Enter dir Repo
+/cd BrynKernel-AOSP
+
+# Excute Bash
+/r chmod a+x gettools.sh
+/r chmod a+x build.clang.sh
+
+# Running Bash
+/r ./gettools.sh
+
+# End
+/r echo "(!) COMPLETE (!)"
+
+# Notice
+/r echo " Now You Can Build or Any "
